be1b700cdcee25e96c0b0752e653e8140f3466f7
mabellemos <mils2@aluno.ifal.edu.br>
2022-07-27 01:25:40 +0000

    Resolução de problemas na saída das informações

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests 
 from pprint import pprint
 
-chave_API = '3cf17c6b8406f27838e6922c0ebde8aa'
+API_chave = '3cf17c6b8406f27838e6922c0ebde8aa'
 
 cidade = input('Informe a cidade: ')
 
-base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+chave_API
+base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_chave+"&q="+cidade
 
 clima = requests.get(base_url).json()
 
