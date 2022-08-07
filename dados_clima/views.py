d5612425a7fc2767d38e15b2834a61f681ba5a03
Maria Izabel <98924290+mabellemos@users.noreply.github.com>
2022-08-07 18:45:54 -0300

    Add files via upload

---
+++ dados_clima/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+import requests 
+from pprint import pprint
+
+def index(request):
+    context = {}
+    if request.method == "POST":
+        API_chave = '3cf17c6b8406f27838e6922c0ebde8aa'
+
+        cidade = request.POST.get('cidade', None)
+        base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_chave+"&q="+cidade
+
+        clima = requests.get(base_url).json
+        context['clima'] = clima
+    return render(request, "index.html", context=context)
