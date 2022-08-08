c2f7b8df82a39b2171b157167f06e8029301a9d2
mabellemos <mils2@aluno.ifal.edu.br>
2022-08-08 14:55:30 +0000

    Identação final

--- static/js/index.js
+++ static/js/index.js
@@ -5,16 +5,16 @@ var chuva = document.getElementById('chuva');
 var tempo = document.getElementById('tempo');
 
 function mudarEstado(){
-        if((texto == "Céu limpo") || (texto == "Céu claro") || (texto == "Intensidade da luz") || (texto == "Poucas nuvens")){
-            document.getElementById('sol').style.display = 'block';
-        }
-        else if((texto == "Nuvens nubladas") || (texto == "Nuvens quebradas") || (texto == "Nuvens dispersas")){
-            document.getElementById('nublado').style.display = 'block';
-        }
-        else if(texto == "Chuva"){
-            document.getElementById('chuva').style.display = 'block';
-        }
-        else {
-            document.getElementById('tempo').style.display = 'block';
-        }
+    if((texto == "Céu limpo") || (texto == "Céu claro") || (texto == "Intensidade da luz") || (texto == "Poucas nuvens")){
+        document.getElementById('sol').style.display = 'block';
+    }
+    else if((texto == "Nuvens nubladas") || (texto == "Nuvens quebradas") || (texto == "Nuvens dispersas")){
+        document.getElementById('nublado').style.display = 'block';
+    }
+    else if(texto == "Chuva"){
+        document.getElementById('chuva').style.display = 'block';
+    }
+    else {
+        document.getElementById('tempo').style.display = 'block';
+    }
 }
